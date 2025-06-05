cc189adc642b36c0294aef786bc383b9e585c8e5
Blaise O'Mara <blaisexomara@gmail.com>
2025-06-05 15:18:27 -0400

    Adding files to upload to each pico

--- OmniClimb/micropython/wireless/config_mqtt.py
+++ OmniClimb/micropython/wireless/config_mqtt.py
@@ -1,6 +1,6 @@
 wifi_ssid = 'BROPHY-TECH'
 wifi_password = 'omniclimb'
-mqtt_clientID = b'pico1'
+mqtt_clientID = 'pico1'
 mqtt_server = b'687c1c8f2d174ae3830815475acd404f.s1.eu.hivemq.cloud'
 mqtt_port = 8883
 mqtt_username = b'OmniClimb'
